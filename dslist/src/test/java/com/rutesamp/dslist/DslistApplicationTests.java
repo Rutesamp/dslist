fa3b212b361103a0ef085287cf575cee7c43b94e
Rutesamp <sampaiorute8@gmail.com>
2025-08-20 16:22:54 -0300

    Adicionando o projeto dslist como uma pasta

---
+++ dslist/src/test/java/com/rutesamp/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutesamp.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
