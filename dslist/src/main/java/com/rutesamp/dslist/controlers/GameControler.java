b09ad534075af556fe3758e54dd0c64afb235020
Rutesamp <sampaiorute8@gmail.com>
2025-08-21 22:57:10 -0300

    Domain model

--- dslist/src/main/java/com/rutesamp/dslist/controlers/GameControler.java
+++ dslist/src/main/java/com/rutesamp/dslist/controlers/GameControler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rutesamp.dslist.entities.Game;
+import com.rutesamp.dslist.dto.GameMinDTO;
 import com.rutesamp.dslist.services.GameService;
 
 @RestController
@@ -18,8 +18,8 @@ public class GameControler {
 	private GameService gameService;
 	
 	@GetMapping
-	public List<Game> findAll() {
-		List<Game> result = gameService.findA11();
+	public List<GameMinDTO> findAll() {
+		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
 } 
--- dslist/src/main/java/com/rutesamp/dslist/services/GameService.java
+++ dslist/src/main/java/com/rutesamp/dslist/services/GameService.java
@@ -15,9 +15,10 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	public List<Game> findA11() {
-	     var result = gameRepository.findAll();
-	     return result;
+	public List<GameMinDTO> findAll() {
+	     List<Game> result = gameRepository.findAll();
+	     List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+	     return dto;
 	}
 	
 }
