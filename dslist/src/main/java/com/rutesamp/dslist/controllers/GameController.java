e6d59d2fb0294e13ec64ee2f5991773d6e626c4b
Rutesamp <sampaiorute8@gmail.com>
2025-08-22 00:13:53 -0300

    GET game by id

--- dslist/src/main/java/com/rutesamp/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/rutesamp/dslist/controllers/GameController.java
@@ -1,21 +1,29 @@
-package com.rutesamp.dslist.controlers;
+package com.rutesamp.dslist.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rutesamp.dslist.dto.GameDTO;
 import com.rutesamp.dslist.dto.GameMinDTO;
 import com.rutesamp.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
-public class GameControler {
+public class GameController {
 
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+	}		
 	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
--- dslist/src/main/java/com/rutesamp/dslist/services/GameService.java
+++ dslist/src/main/java/com/rutesamp/dslist/services/GameService.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
+import com.rutesamp.dslist.dto.GameDTO;
 import com.rutesamp.dslist.dto.GameMinDTO;
 import com.rutesamp.dslist.entities.Game;
 import com.rutesamp.dslist.repositories.GameRepository;
@@ -15,6 +19,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Game not found with ID: " + id)
+        );
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 	     List<Game> result = gameRepository.findAll();
 	     List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
